0a7a4728d993b4a9a1cfce67422cbd5bb28a29d5
Mary Arbachauskas <mary.arbachauskas@gmail.com>
2019-11-20 14:29:55 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -26,7 +26,7 @@ class Song
     self.artist = artist
     artist.add_song(self)
   end
-  # 
+  #
   # def self.new_by_filename(file)
   #   song_info = file.chomp(".mp3").split(" - ")
   #   song = Song.new(song_info[1])
