000b4ff7faa9d548ee7d690c24ca4b4476604e43
Mary Arbachauskas <mary.arbachauskas@gmail.com>
2019-11-20 15:12:45 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -31,10 +31,10 @@ class Artist
   #   @songs << song_one
   # end
   def add_song(song)
-  @songs << song
-  song.artist = self
-  song
-end
+   @songs << song
+   song.artist = self
+   song
+  end
 
   def print_songs
     Song.all.each { |song| puts song.name}
