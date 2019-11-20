2e5380f4618e522d9b77243a1910706fc67b7a63
Mary Arbachauskas <mary.arbachauskas@gmail.com>
2019-11-20 14:28:04 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,34 +1,50 @@
-class Song
-  @@all=[]
-
-  attr_accessor :artist, :name
+# class Song
+  attr_accessor :name, :artist
 
   def initialize(name)
-    @name=name
-    @artist=artist
-    @@all << self
-  end
-
-  def self.all
-    @@all
+    @name = name
   end
 
   def self.new_by_filename(file)
-    song_info = file.chomp(".mp3").split(" - ")
-    song = Song.new(song_info[1])
-    song.artist_name = song_info[0]
+    artist_name = file.split(" - ")[0]
+    song_name = file.split(" - ")[1]
+    song = Song.new(song_name)
+    song.artist_name = artist_name
     song
   end
 
-
   def artist_name=(name)
-    self.artist = Artist.find_or_create_by_name(name)
-    self.artist.add_song(self)
+    artist = Artist.find_or_create_by_name(name)
+    self.artist = artist
+    artist.add_song(self)
   end
-  #  self.artist = Artist.find_or_create_by_name(name)
-  #  self.artist.add_song(self)
 
-
-
-
-end
+# class Song
+#   @@all=[]
+# 
+#   attr_accessor :artist, :name
+# 
+#   def initialize(name)
+#     @name=name
+#     @artist=artist
+#     @@all << self
+#   end
+# 
+#   def self.all
+#     @@all
+#   end
+# 
+#   def self.new_by_filename(file)
+#     song_info = file.chomp(".mp3").split(" - ")
+#     song = Song.new(song_info[1])
+#     song.artist_name = song_info[0]
+#     song
+#   end
+# 
+# 
+#   def artist_name=(name)
+#     self.artist = Artist.find_or_create_by_name(name)
+#     self.artist.add_song(self)
+#   end
+# 
+# end
