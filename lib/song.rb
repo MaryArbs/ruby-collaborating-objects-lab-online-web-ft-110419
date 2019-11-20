c229cb677181022f9bd22105b97052530d0a8d71
Mary Arbachauskas <mary.arbachauskas@gmail.com>
2019-11-20 14:28:07 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -21,30 +21,30 @@
 
 # class Song
 #   @@all=[]
-# 
+#
 #   attr_accessor :artist, :name
-# 
+#
 #   def initialize(name)
 #     @name=name
 #     @artist=artist
 #     @@all << self
 #   end
-# 
+#
 #   def self.all
 #     @@all
 #   end
-# 
+#
 #   def self.new_by_filename(file)
 #     song_info = file.chomp(".mp3").split(" - ")
 #     song = Song.new(song_info[1])
 #     song.artist_name = song_info[0]
 #     song
 #   end
-# 
-# 
+#
+#
 #   def artist_name=(name)
 #     self.artist = Artist.find_or_create_by_name(name)
 #     self.artist.add_song(self)
 #   end
-# 
+#
 # end
