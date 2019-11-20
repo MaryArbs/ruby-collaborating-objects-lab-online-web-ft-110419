c3c46aa927b213650d19aca491615ff8e4e02b9a
Mary Arbachauskas <mary.arbachauskas@gmail.com>
2019-11-20 14:36:24 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -39,36 +39,36 @@
 end
 # class Artist
 #   attr_accessor :name, :songs
-# 
+#
 #   @@all=[]
-# 
+#
 #   def initialize(name)
 #     @name = name
 #     @@all << self
-# 
+#
 #   end
-# 
+#
 #   def self.all
 #     @@all
 #   end
-# 
+#
 #   def songs
 #     @songs
 #   end
-# 
+#
 #   def save
 #     @@all << self
 #     self
 #   end
-# 
+#
 #   def songs
 #    Song.all.select {|song| song.artist == self}
 #   end
-# 
+#
 #   def add_song(song_one)
 #    song_one.artist = self
 #   end
-# 
+#
 #   def self.find_or_create_by_name(name)   #class method uses find/create class
 #                                             #methods to detect or create instances
 #       if self.find(name)
@@ -77,11 +77,11 @@ end
 #         self.create(name)
 #       end
 #     end
-# 
+#
 #     def self.find(name)     #class method detects instances from @@all class variable
 #       self.all.detect { |artist| artist.name == name }
 #     end
-# 
+#
 #     def self.create(name)
 #       artist = Artist.new(name)
 #       artist.save
@@ -93,12 +93,12 @@ end
 #   # #   finds or creates an artist by name maintaining uniqueness of objects by name property
 #   # #   Creates new instance of Artist if none exist
 #   # end
-# 
-# 
+#
+#
 #     def print_songs
 #       self.songs.each {puts |song| song.name}
 #     end
-# 
-# 
-# 
+#
+#
+#
 # end
