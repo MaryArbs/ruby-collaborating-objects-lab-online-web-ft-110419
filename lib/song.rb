eea1af140b45b4ecf6446e1595210799eee5dafa
Mary Arbachauskas <mary.arbachauskas@gmail.com>
2019-11-20 14:34:36 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -22,17 +22,16 @@ class Song
     song
   end
   #
+  # def artist_name=(name)
+  #   artist = Artist.find_or_create_by_name(name)
+  #   self.artist = artist
+  #   artist.add_song(self)
+  # end
+
   def artist_name=(name)
-    artist = Artist.find_or_create_by_name(name)
-    self.artist = artist
+    self.artist = Artist.find_or_create_by_name(name)
     artist.add_song(self)
   end
-
-
   #
-  # def artist_name=(name)
-  #   self.artist = Artist.find_or_create_by_name(name)
-  #   self.artist.add_song(self)
-  # end
 
 end
